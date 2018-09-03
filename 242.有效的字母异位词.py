1fe7d9273a8faac58f86cf82290f774a4ea74f5e
yg271334757 <35893186+yg271334757@users.noreply.github.com>
2018-09-03 14:21:23 +0100

    Update 242.有效的字母异位词.py

--- 242.有效的字母异位词.py
+++ 242.有效的字母异位词.py
@@ -7,12 +7,12 @@ class Solution:
         """
         if len(s) != len(t):
             return False
-        stack = ''
-        for i in s:
-            if i not in stack:
-                stack += i
-            else:
-                continue
+        #stack = ''
+        for i in set(s):
+            #if i not in stack:
+                #stack += i
+            #else:
+                #continue
             if s.count(i) != t.count(i): 
                return False
         return True
