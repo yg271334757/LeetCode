e32e9f7a649feb4a1007c72992ec75c7efff0f5b
yg271334757 <271334757@qq.com>
2018-09-05 11:12:04 +0100

    two pointer

--- 345翻转字符串中的元音字母.py
+++ 345翻转字符串中的元音字母.py
@@ -1,19 +1,23 @@
-def reverseVowels(self, s):
+class Solution:
+    def reverseVowels(self, s):
         """
         :type s: str
         :rtype: str
         """
-        if s == '':
-            return s
-        vowels = 'aeiouAEIOU'
-        stack = []
-        ans = ''
-        for item in s:
-            if item in vowels:
-                stack.append(item)
-        for item in s:
-            if item in vowels:
-                ans += stack.pop()
+        vow = 'aeiouAEIOU'
+        s = list(s)
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if s[l] in vow and s[r] in vow:
+                s[l], s[r] = s[r], s[l]
+                r -= 1
+                l += 1
+            elif s[l] in vow and s[r] not in vow:
+                r -= 1
+            elif s[l] not in vow and s[r] in vow:
+                l += 1
             else:
-                ans += item
-        return ans
+                l += 1
+                r -= 1
+        return ''.join(s)
