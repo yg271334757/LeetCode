a1bfce53d67b6363d713a4cdc5baa2d6b0f3de7d
yg271334757 <271334757@qq.com>
2018-09-12 14:44:44 +0100

    Type 'set' can be minus

---
+++ 448找到所有在数组中消失的数字.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return list(set(list(range(1,len(nums) + 1))) - set(nums))
