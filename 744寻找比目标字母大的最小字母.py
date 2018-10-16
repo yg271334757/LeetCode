e95962cf0e9bdd2ae6bf697901cc6b94d1b579b9
yg271334757 <271334757@qq.com>
2018-10-16 10:52:36 +0100

    easy

---
+++ 744寻找比目标字母大的最小字母.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        keys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        values = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+        dictmap = dict(zip(keys, values))
+        for i in sorted(set(letters)):
+            if dictmap[i] > dictmap[target]:
+                return i
+        return letters[0]
