c6672f7774c3c5d73bb975ba5e52eb704e8da901
yg271334757 <271334757@qq.com>
2018-10-16 16:27:39 +0100

    easy

---
+++ 796旋转字符串.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotateString(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+        if len(A) != len(B):
+            return False
+        return B in A * 2
