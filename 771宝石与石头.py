9d5c81cd41eac9e379683a04936295349de078e0
yg271334757 <271334757@qq.com>
2018-10-16 14:07:24 +0100

    easy

---
+++ 771宝石与石头.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0
+        for i in J:
+            count += S.count(i)
+        return count
