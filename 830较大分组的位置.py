b66570b020cfe3c0512cb4548d06c5e5c850e7ca
yg271334757 <271334757@qq.com>
2018-10-17 21:01:50 +0100

    Two pointer

---
+++ 830较大分组的位置.py
@@ -0,0 +1,20 @@
+class Solution:
+    def largeGroupPositions(self, S):
+        """
+        :type S: str
+        :rtype: List[List[int]]
+        """
+        l = 0
+        r = 1
+        res = []
+        while r < len(S):
+            if S[l] != S[r]:
+                if r - l >= 3:
+                    res.append([l, r - 1])
+                l = r
+                r += 1
+            else:
+                r += 1
+        if r - l >= 3:
+            res.append([l, r-1])
+        return res
